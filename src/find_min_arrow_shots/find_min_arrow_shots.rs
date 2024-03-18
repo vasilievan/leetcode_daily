eafaf151c5db2c32c6a19af9ecd56f7ba57de4c8
Aleksey Vasilev <vasiliev.an@edu.spbstu.ru>
2024-03-18 22:27:50 +0200

    Initial commit

---
+++ src/find_min_arrow_shots/find_min_arrow_shots.rs
@@ -0,0 +1,14 @@
+pub fn insert(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
+    if intervals.is_empty() {
+        return vec![new_interval];
+    }
+    if new_interval.is_empty() {
+        return intervals;
+    }
+
+    let mut intervals_copy = intervals.clone();
+
+    intervals.map
+
+    return intervals;
+}
